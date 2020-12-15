90653edea3b5499f350f2a4b234ab791eb69f769
Julian Klug <tensu.wave@gmail.com>
2020-12-15 21:47:32 +0100

    Use reference bounding box and voxel space

--- preprocessing/mlab/normalisation_job.m
+++ preprocessing/mlab/normalisation_job.m
@@ -13,19 +13,19 @@ for idx = 1:numel(images_to_co_normalise)
     images_to_resample{end +1, 1} = images_to_co_normalise{idx};
 end
 
+[bb,vx]=spm_get_bbox(reference{1,1});
+
 matlabbatch{1}.spm.spatial.normalise.estwrite.subj.vol = main_image; 
 matlabbatch{1}.spm.spatial.normalise.estwrite.subj.resample = images_to_resample;
 
 matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasreg = 0.0001;
 matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasfwhm = 60;
 matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.template = reference;
-% matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.tpm = {'/Users/jk1/matlab/toolboxes/spm12/tpm/TPM.nii'};
 matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.affreg = 'mni';
 matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.reg = [0 0.001 0.5 0.05 0.2];
 matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.fwhm = 0;
 matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.samp = 3;
-matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.bb = [-78 -112 -70
-                                                             78 76 85];
-matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.vox = [2 2 2];
+matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.bb = bb;
+matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.vox = vx;
 matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.interp = 4;
 matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.prefix = '/norm/norm_';
